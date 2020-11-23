479b3b1fbabfb79e5c81c0513662c622b4bcc69a
Duncan Thacker <duncan.thacker@atropos.co.uk>
2020-11-23 23:41:01 +0000

    inset shadow on day window

--- src/DayWindow.js
+++ src/DayWindow.js
@@ -69,6 +69,7 @@ export default function DayWindow({
       <button
         onClick={handleOldClick}
         style={{
+          boxShadow: "inset 10px 10px 18px rgba(0,0,0,0.6)",
           cursor: "pointer",
           width: "100%",
           height: "100%",
