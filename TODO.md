8d63adcbbc4c8f009aa1cd4561c6d30a1a282c91
Duncan Thacker <duncan.thacker@atropos.co.uk>
2020-11-28 21:53:18 +0000

    warning on small screens remove unused deps

--- TODO.md
+++
@@ -1,5 +0,0 @@
-* favicons etc
-* mobile responsiveness
-* day windows should react to browser resize
-* proper menu
-* about info
--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Typography } from "@material-ui/core";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const CENTRE_STYLE = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+};
+
+const ROOT = document.getElementById("root");
+
+if (window.innerWidth < 1400 || window.innerHeight < 800) {
+  ReactDOM.render(
+    <div style={CENTRE_STYLE}>
+      <Typography variant="h6" style={{ maxWidth: 500 }}>
+        Sorry, the Calendar needs a bigger screen and doesn't work well on
+        mobile devices. Your screen should be at least{" "}
+        <strong>1400 x 800</strong>.
+      </Typography>
+    </div>,
+    ROOT
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    ROOT
+  );
+}
